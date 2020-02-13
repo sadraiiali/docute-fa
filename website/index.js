bdfaab2b1ab17f8d2d92644dcf844b8cf3643ed6
Ali Sadraii <sadraiiali@gmail.com>
2020-02-13 13:10:40 +0330

    Default font added to docute-fa

--- website/index.js
+++ website/index.js
@@ -3,73 +3,37 @@ import Docute from '../src'
 import prismLanguages from '../src/utils/prismLanguages'
 import ColorBox from './components/ColorBox.vue'
 
-// Custom Font
-const style = document.createElement('style')
-style.textContent = `
-                  @font-face {
-                    font-family: Sahel;
-                    src: url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel.eot');
-                    src: url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel.eot?#iefix') format('embedded-opentype'),
-                         url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel.woff') format('woff'),
-                         url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel.ttf') format('truetype');
-                    font-weight: normal;
-                  }
-                  
-                  @font-face {
-                    font-family: Sahel;
-                    src: url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel-Bold.eot');
-                    src: url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel-Bold.eot?#iefix') format('embedded-opentype'),
-                         url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel-Bold.woff') format('woff'),
-                         url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel-Bold.ttf') format('truetype');
-                    font-weight: bold;
-                  }
-                  
-                  @font-face {
-                    font-family: Sahel;
-                    src: url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel-Black.eot');
-                    src: url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel-Black.eot?#iefix') format('embedded-opentype'),
-                         url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel-Black.woff') format('woff'),
-                         url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel-Black.ttf') format('truetype');
-                    font-weight: 900;
-                  }
-                  body{
-                         font: 16px/1.7 Sahel,-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif!important;
-                  }
-            `
-document.head.appendChild(style)
-//
 
 new Docute({
-  target: 'app',
-  title: 'مستندات شبیه‌ساز دوبعدی فوتبال',
-  highlight: ['c', 'cpp', 'bash', 'json', 'markdown'],
-  plugins: [process.env.NODE_ENV === 'production'].filter(Boolean),
-  editLinkBase: 'https://github.com/egoist/docute/tree/master/website/docs',
-  editLinkText: 'این صفحه را در گیتهاب ویرایش کنید.',
-  router: {
-    mode: 'history'
-  },
-  detectSystemDarkTheme: true,
-  darkThemeToggler: true,
-  sourcePath: '/',
-  componentMixins: [
-    {
-      data() {
-        return {
-          builtinLanguages: prismLanguages.builtin,
-          deps: __DEPS__
-        }
-      },
-      methods: {
-        insertCustomFontsCSS() {
-          const ID = 'custom-fonts-css'
-          const existing = document.getElementById(ID)
-          if (existing) {
-            existing.parentNode.removeChild(existing)
-          } else {
-            const style = document.createElement('style')
-            style.id = ID
-            style.textContent = `
+    target: 'app',
+    title: 'مستندات شبیه‌ساز دوبعدی فوتبال',
+    highlight: ['c', 'cpp', 'bash', 'json', 'markdown'],
+    plugins: [process.env.NODE_ENV === 'production'].filter(Boolean),
+    editLinkBase: 'https://github.com/egoist/docute/tree/master/website/docs',
+    editLinkText: 'این صفحه را در گیتهاب ویرایش کنید.',
+    router: {
+        mode: 'history'
+    },
+    detectSystemDarkTheme: true,
+    darkThemeToggler: true,
+    sourcePath: '/',
+    componentMixins: [{
+        data() {
+            return {
+                builtinLanguages: prismLanguages.builtin,
+                deps: __DEPS__
+            }
+        },
+        methods: {
+            insertCustomFontsCSS() {
+                const ID = 'custom-fonts-css'
+                const existing = document.getElementById(ID)
+                if (existing) {
+                    existing.parentNode.removeChild(existing)
+                } else {
+                    const style = document.createElement('style')
+                    style.id = ID
+                    style.textContent = `
                   @font-face {
                     font-family: Sahel;
                     src: url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel.eot');
@@ -78,7 +42,7 @@ new Docute({
                          url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel.ttf') format('truetype');
                     font-weight: normal;
                   }
-                  
+
                   @font-face {
                     font-family: Sahel;
                     src: url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel-Bold.eot');
@@ -87,7 +51,7 @@ new Docute({
                          url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel-Bold.ttf') format('truetype');
                     font-weight: bold;
                   }
-                  
+
                   @font-face {
                     font-family: Sahel;
                     src: url('https://cdn.fontcdn.ir/Font/Persian/Sahel/Sahel-Black.eot');
@@ -100,69 +64,64 @@ new Docute({
                          font: 16px/1.7 Sahel,-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif!important;
                   }
             `
-            document.head.appendChild(style)
-          }
+                    document.head.appendChild(style)
+                }
+            }
+        },
+        components: {
+            ColorBox
         }
-      },
-      components: {
-        ColorBox
-      }
-    }
-  ],
-  versions: {
-    Agent2D: {
-      link: '/'
+    }],
+    versions: {
+        Agent2D: {
+            link: '/'
+        },
+        Librcsc: {
+            link: 'https://v3.docute.org'
+        }
     },
-    Librcsc: {
-      link: 'https://v3.docute.org'
-    }
-  },
-  nav: [
-    {
-      title: 'مستندات',
-      link: '/'
-    },
-    {
-      title: 'نوشته‌ها',
-      link: 'https://github.com/egoist/docute'
-    }
-  ],
-  sidebar: [
-    {
-      title: 'پایه‌ای',
-      children: [
+    nav: [{
+            title: 'مستندات',
+            link: '/'
+        },
         {
-          title: 'شبیه‌ساز دوبعدی فوتبال ؟',
-          link: '/'
+            title: 'نوشته‌ها',
+            link: 'https://github.com/egoist/docute'
         }
-      ]
-    },
-    {
-      title: 'پیشرفته',
-      children: []
-    },
-    {
-      title: 'منابع',
-      link: '/resources'
-    },
-    {
-      title: 'پس گفتار',
-      link: '/credits'
-    }
-  ],
-  footer: `
+    ],
+    sidebar: [{
+            title: 'پایه‌ای',
+            children: [{
+                title: 'شبیه‌ساز دوبعدی فوتبال ؟',
+                link: '/'
+            }]
+        },
+        {
+            title: 'پیشرفته',
+            children: []
+        },
+        {
+            title: 'منابع',
+            link: '/resources'
+        },
+        {
+            title: 'پس گفتار',
+            link: '/credits'
+        }
+    ],
+    footer: `
   <div style="border-top:1px solid var(--border-color);padding-top:30px;margin: 40px 0;color:#999999;font-size: .9rem;">
   &copy; ${new Date().getFullYear()}.
-   ساخته شده با ❤️ در 
+   ساخته شده با ❤️ در
    <a href="https://kn2c.ir" target="_blank">
    KN2C Robotics Lab.</a>
-    
-    برگرفته از 
+
+    برگرفته از
     <a href="https://docute.org/">DOCUTE</a>
      </div>
   `,
-  banner: {
-    template: html`
+    banner: {
+        template: html `
       <div class="docute-banner">
         <note :label="false">
           ما را حمایت کنید
@@ -174,18 +133,18 @@ new Docute({
 
       </div>
     `,
-    components: {}
-  }
+        components: {}
+    }
 })
 
 Vue.component('ReverseText', {
-  props: {
-    text: {
-      type: String,
-      required: true
-    }
-  },
-  template: html`
+    props: {
+        text: {
+            type: String,
+            required: true
+        }
+    },
+    template: html `
     <div class="reverse-text">
       {{ reversedText }}
       <v-style>
@@ -194,16 +153,16 @@ Vue.component('ReverseText', {
       </v-style>
     </div>
   `,
-  computed: {
-    reversedText() {
-      return this.text
-        .split('')
-        .reverse()
-        .join('')
+    computed: {
+        reversedText() {
+            return this.text
+                .split('')
+                .reverse()
+                .join('')
+        }
     }
-  }
 })
 
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/sw.js')
 }
